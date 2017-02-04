5298ba02b1c7572bbb33df3a551f17f43761b78d
WhatAKitty <104xuqiang@163.com>
2017-02-04 22:40:14 +0800

    Login request events group

--- src/utils/permission.js
+++ src/utils/permission.js
@@ -9,6 +9,15 @@ import { Navigation } from 'react-native-navigation';
 //   return true;
 // }
 
+let isLogin = false;
+let eventGroup = [];
+
+const eventGroupTrigger = () => {
+  eventGroup.forEach(evt => {
+    evt && 'function' === typeof evt && evt();
+  });
+}
+
 export const userRequiredAndDispatch = async (cb) => {
   Navigation.showModal({
     screen: 'app.login',
@@ -22,7 +31,13 @@ export const userRequiredAndDispatch = async (cb) => {
 
 export const checkAuth = async (err, cb) => {
   if (err.status === 401) {
-    await userRequiredAndDispatch(cb);
+    // 在近乎同一时刻进行登录请求竞争，选择第一个获胜者，将其余请求者的回调请求置入事件队列，当登录成功后，完成队列里的事件执行
+    eventGroup.push(cb);
+    if (isLogin) {
+      return err;
+    }
+    isLogin = false;
+    await userRequiredAndDispatch(eventGroupTrigger);
   }
   return err;
 }
