254081ea618e609029dd3d262c66fa6b32fc4b0d
WhatAKitty <104xuqiang@163.com>
2017-02-16 00:48:28 +0800

    Upgrade to ReactNative 0.41.2 ☺

--- android/app/src/main/java/com/vipshare/MainApplication.java
+++ android/app/src/main/java/com/vipshare/MainApplication.java
@@ -1,29 +1,46 @@
 package com.vipshare;
 
-import android.support.annotation.Nullable;
+import android.app.Application;
+import android.util.Log;
 
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.reactnativenavigation.NavigationApplication;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends NavigationApplication {
+public class MainApplication extends Application implements ReactApplication {
 
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    public boolean isDebug() {
-        return BuildConfig.DEBUG;
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
     }
 
-    @Nullable
     @Override
-    public List<ReactPackage> createAdditionalReactPackages() {
-        return Arrays.<ReactPackage>asList(
-                new ReactNativeConfigPackage(),
-                new VectorIconsPackage()
-        );
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new VectorIconsPackage(),
+            new ReactNativeConfigPackage()
+      );
     }
+  };
 
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
 }
--- ios/VIPShare/AppDelegate.m
+++ ios/VIPShare/AppDelegate.m
@@ -9,10 +9,8 @@
 
 #import "AppDelegate.h"
 
-#import "RCCManager.h"
-
-#import "RCTBundleURLProvider.h"
-#import "RCTRootView.h"
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 
 @implementation AppDelegate
 
@@ -20,24 +18,19 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
-//  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                      moduleName:@"VIPShare"
+                                               initialProperties:nil
+                                                   launchOptions:launchOptions];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  self.window.backgroundColor = [UIColor whiteColor];
-  [[RCCManager sharedInstance] initBridgeWithBundleURL:jsCodeLocation];
-
-  // RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-  //                                                     moduleName:@"VIPShare"
-  //                                              initialProperties:nil
-  //                                                  launchOptions:launchOptions];
-  // rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
-  // self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  // UIViewController *rootViewController = [UIViewController new];
-  // rootViewController.view = rootView;
-  // self.window.rootViewController = rootViewController;
-  // [self.window makeKeyAndVisible];
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
   return YES;
 }
 
