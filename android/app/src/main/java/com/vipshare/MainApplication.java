7be3b8a584f5b8a6ccdd6491176cddbbe6711864
WhatAKitty <104xuqiang@163.com>
2017-02-16 00:25:36 +0800

    Add navigation to android

--- android/app/src/main/java/com/vipshare/MainApplication.java
+++ android/app/src/main/java/com/vipshare/MainApplication.java
@@ -1,46 +1,29 @@
 package com.vipshare;
 
-import android.app.Application;
-import android.util.Log;
+import android.support.annotation.Nullable;
 
-import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactnativenavigation.NavigationReactPackage;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
+import com.reactnativenavigation.NavigationApplication;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    protected boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+public class MainApplication extends NavigationApplication {
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage(),
-            new NavigationReactPackage()
-      );
+    public boolean isDebug() {
+        return BuildConfig.DEBUG;
     }
-  };
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
+    @Nullable
+    @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return Arrays.<ReactPackage>asList(
+                new ReactNativeConfigPackage(),
+                new VectorIconsPackage()
+        );
+    }
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
