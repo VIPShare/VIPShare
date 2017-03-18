5c00d6dc418a5f0af97051fe9e9efd8cc3e64775
WhatAKitty <104xuqiang@163.com>
2017-03-19 00:16:07 +0800

    PullSelect upgrade.

--- src/components/PullSelect/index.js
+++ src/components/PullSelect/index.js
@@ -8,19 +8,33 @@ class PullSelect extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      selected: null,
+    }
+
+    this.onSelect = this.onSelect.bind(this);
     this.onPress = this.onPress.bind(this);
   }
 
+  onSelect(selected) {
+    this.setState({
+      selected,
+    });
+    this.props.onChangeText(selected);
+  }
+
   onPress() {
-    this.props.navigator.showModal({
-      screen: this.props.screen,
-      title: this.props.title,
-      animationType: 'slide-up',
+    this.props.navigation.navigate(this.props.screen, {
+      onSelect: this.onSelect,
     });
   }
 
   render() {
-    const { placeholder } = this.props;
+    let { placeholder } = this.props;
+    if (this.state.selected) {
+      placeholder = this.state.selected.title;
+    }
+    
     return (
       <TouchableHighlight onPress={ this.onPress } underlayColor="#eee" >
         <View style={ styles.buttonWrapper }>
@@ -38,6 +52,11 @@ PullSelect.propTypes = {
   placeholder: PropTypes.string.isRequired,
   screen: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  onChangeText: PropTypes.func.isRequired,
+}
+
+PullSelect.defaultProps = {
+  onChangeText: () => {},
 }
 
 export default PullSelect;
