1be68847ebe8bbca6ef7c183f7a67cca86128fad
WhatAKitty <104xuqiang@163.com>
2017-05-07 17:30:33 +0800

    More functional for Page component
    * Add need login show
    * Add Loading show

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,31 +1,53 @@
 import React, { PropTypes, Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 import EmptyView from '../EmptyView';
+
+import { isLoginin } from '../../utils/permission';
+
+import styles from './index.style';
 
 class Page extends Component {
   static propTypes = {
     containerStyle: PropTypes.object.isRequired,
+    needLogin: PropTypes.bool.isRequired,
     enableLoad: PropTypes.bool.isRequired,
     children: PropTypes.node.isRequired,
+
+    init: PropTypes.func.isRequired,
   }
   static defaultProps = {
     containerStyle: {},
-    enableLoad: false,
+    needLogin: true,
+    enableLoad: true,
+    init: () => { },
   }
 
   constructor(props) {
     super(props);
 
-    if (props.enableLoad) {
-      this.state = {
-        loading: true,  // default loading show
-      };
+    this.state = {
+      loginin: false, // default not loginin.
+      loading: true,  // default loading show
+    }
+  }
+
+  async componentDidMount() {
+    if (this.props.needLogin) {
+      if (await isLoginin()) {
+        this.setState({
+          loginin: true,
+        });
+        this.props.init();
+      }
+    } else {
+      this.props.init();
     }
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.props.enableLoad && 'bool' === typeof nextProps.loading && !nextProps.loading) {
+    if (this.props.enableLoad && 'boolean' === typeof nextProps.loading && !nextProps.loading) {
       // while loaded
       this.setState({
         loading: nextProps.loading,
@@ -34,11 +56,26 @@ class Page extends Component {
   }
 
   render() {
+    if (this.props.needLogin && !this.state.loginin) {
+      // show need login page
+      return (
+        <EmptyView
+          tip="Need Login"
+          footer={
+            <Button title="PRESSED TO LOGIN" onPress={() => {
+              this.props.screenProps.redirectLogin();
+            }} />
+          }
+        />
+      );
+    }
     if (this.props.enableLoad && this.state.loading) {
       // show loading page
       return (
         <EmptyView
-          tip="loading"
+          tip=""
+          icon={require('../../assets/loading.gif')}
+          iconStyle={styles.loading.image}
         />
       );
     }
---
+++ src/components/Page/index.style.js
@@ -0,0 +1,10 @@
+import { width } from '../../theme';
+
+export default {
+  loading: {
+    image: {
+      width: width / 2,
+      resizeMode: 'contain',
+    }
+  }
+}
