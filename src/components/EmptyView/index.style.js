f424ebb68a9d88e44566019c3e12ddc13c65403b
WhatAKitty <104xuqiang@163.com>
2017-05-07 17:25:56 +0800

    Remove Flex Component
    use Col, Row and Grid instead of flex

--- src/components/EmptyView/index.style.js
+++ src/components/EmptyView/index.style.js
@@ -1,6 +1,11 @@
 import { colors, size, tip } from '../../theme';
 
 export default {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   tip: tip.title,
   subTip: tip.subTitle,
 }
